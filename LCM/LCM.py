586afa2f2d9fd45db8eb19f749e469f61ba19a35
styalai <127979302+styalai@users.noreply.github.com>
2025-01-10 16:42:18 +0100

    Add files via upload

---
+++ LCM/LCM.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from wtpsplit import SaT
+from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline
+from sonar.inference_pipelines.text import EmbeddingToTextModelPipeline
+
+class Transformer(nn.Module):
+    def __init__(self, embd_dim, dim, layers, heads, device):
+        super().__init__()
+        self.embd_dim = embd_dim
+        self.dim = dim
+        self.layers = layers
+        self.heads = heads
+        
+        self.prenet = nn.Sequential(
+            nn.LayerNorm(embd_dim),
+            nn.Linear(embd_dim, dim)
+        )
+
+        decoder_layer = nn.TransformerDecoderLayer(d_model=dim, nhead=heads)
+        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=layers)
+        self.memory = torch.zeros((1, dim)).to(device)
+        
+        self.postnet = nn.Sequential(
+            nn.Linear(dim, embd_dim),
+            nn.LayerNorm(embd_dim)
+        )
+    def forward(self, x):
+        return self.postnet(self.decoder(self.prenet(x), self.memory))
+
+
+class LCMModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.sat_sm = SaT(config.model_name)
+        print("splitter initialized")
+        
+        self.t2vec_model = TextToEmbeddingModelPipeline(encoder=config.sonar_enc, tokenizer=config.sonar_enc, device=torch.device(config.device))
+        print("t2vec_model initialized")
+        
+        self.transformer = Transformer(config.embd_dim, config.dim, config.layers, config.heads, config.device).to(config.device)
+        print("transformer initialized")
+        
+        self.vec2text_model = EmbeddingToTextModelPipeline(decoder=config.sonar_dec, tokenizer=config.sonar_dec, device=torch.device(config.device))
+        print("vec2text_model initialized")
+    
+    def split_into_concepts(self, text):
+        return self.sat_sm.split(text, threshold=self.config.threshold)
+    
+    def forward(self, embeddings):
+        out_embeddings = self.transformer.forward(embeddings)
+        return out_embeddings
+    
+    def generate(self, text, num_generated_concepts=1):
+        with torch.no_grad():
+            concepts = self.split_into_concepts(text)
+            for c in range(num_generated_concepts):
+                embeddings = self.t2vec_model.predict(concepts, source_lang=self.config.lang)
+                out_embeddings = self.forward(embeddings)
+                next_concept = self.vec2text_model.predict(out_embeddings, target_lang=self.config.lang, max_seq_len=self.config.max_seq_len)
+                concepts.append(next_concept[0])
+        return "".join(concepts)
+    
